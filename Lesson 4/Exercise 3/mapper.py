d2f6cbfc5a2af2034ff18ab1405bbb0686f25906
Anubhav Saxena <anubhavsaxena14992@gmail.com>
2018-04-23 23:04:51 +0530

    Create mapper.py

---
+++ Lesson 4/Exercise 3/mapper.py
@@ -0,0 +1,14 @@
+import sys
+import re
+import csv
+
+reader = csv.reader(sys.stdin, delimiter='\t')
+delimiters = ['.',',','$',';','!','?',':','"','(',')','[',']','<','>','#','=','-','/',' ']
+regex = '|'.join(map(re.escape, delimiters))
+for line in reader:
+    node_id = line[0]
+    if node_id == "id":
+        continue
+    body = re.split(regex, line[4])
+    for token in body:
+        print "{0}\t{1}".format(node_id, token)
