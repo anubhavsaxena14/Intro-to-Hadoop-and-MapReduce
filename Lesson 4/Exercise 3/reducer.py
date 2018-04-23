0cbde725c4f8c560cc9ceb07ce7f89b78e29d3c8
Anubhav Saxena <anubhavsaxena14992@gmail.com>
2018-04-23 23:05:38 +0530

    Create reducer.py

---
+++ Lesson 4/Exercise 3/reducer.py
@@ -0,0 +1,15 @@
+import sys
+import re
+import collections
+
+inverted_index = collections.defaultdict(list)
+for line in sys.stdin:
+    data = line.strip().split("\t")
+    if len(data) != 2:
+        continue
+
+    word = data[1].lower()
+    inverted_index[word].append(data[0])
+
+for word in inverted_index:
+    print "{0}\t{1}\t{2}".format(word, len(inverted_index[word]), inverted_index[word])
